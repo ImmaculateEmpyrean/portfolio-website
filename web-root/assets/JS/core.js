828bde6da6738655e162dec66c600e8d1ee584f9
veeru <immaculateempyrean@gmail.com>
2021-04-30 08:13:04 +0530

    rudimentary javascript seems to be working.

---
+++ web-root/assets/JS/core.js
@@ -0,0 +1,24 @@
+var onBeginFunctions = [];
+
+document.addEventListener("DOMContentLoaded",function(){
+    forEach(onBeginFunctions,function(callback){
+        callback();
+    });
+});
+
+function onBegin(callback)
+{
+    onBeginFunctions.push(callback);
+}
+
+function forEach(array,callback)
+{
+    for(var i = 0;i< array.length;i++)
+    {
+        callback(array[i]);
+    }
+}
+
+onBegin(function(){
+    alert("hi");
+});
